26b9aa52fde04e10395f9d8d56a2986aa5720121
Cinemavillaz <87010768+Cinemavillaz@users.noreply.github.com>
2021-07-18 12:12:36 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -203,9 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Found {(len_results)} 𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐌𝐨𝐯𝐢𝐞:- {mention}
-𝐑𝐞𝐬𝐮𝐥𝐭𝐬 𝐅𝐨𝐮𝐧𝐝:- {count}
-𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐁𝐲:- {mention_name}
+                text=f"Found {(len_results)} 
 : <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
